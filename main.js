80b5b5dcb8b5c58a35d71b0b24744ac26d12d5be
anchanel1003 <anchicken99@gmail.com>
2022-06-10 09:10:55 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,187 @@
+<div class="swiper mySwiper">
+        <div class="swiper-wrapper">
+            <div class="swiper-slide">
+            <div class="content-item">
+                        <div class="item-img item3-tablet">
+                           <img src="./styles/img/Rectangle 125.png" alt="">
+                           <div class="discount-item">50%</div>
+                           <div class="img-icon1">
+                               <i class="fa-solid fa-chevron-left"></i>
+                           </div>
+                        </div>
+                        <div class="name-glass name-change">
+                        <p>GL - 380GL001</p>
+                        <div class="full-color">
+                            <div class="hinh-tron color1"></div>
+                            <div class="hinh-tron color2 change-color"></div>
+                            <div class="hinh-tron color3"></div>
+                            <div class="hinh-tron color4"></div>
+                            <div class="hinh-tron color5"></div>
+                        </div>
+                        </div>
+                        <div class="price-glass change-price">
+                           <p>170.000 đ</p>
+                           <p>250.000 đ</p>
+                        </div>
+            </div>
+            </div>
+
+            <div class="swiper-slide">
+            <div class="content-item">
+                        <div class="item-img item3-tablet">
+                           <img src="./styles/img/Rectangle 125.png" alt="">
+                           <div class="discount-item">50%</div>
+                           <div class="img-icon1">
+                               <i class="fa-solid fa-chevron-left"></i>
+                           </div>
+                        </div>
+                        <div class="name-glass name-change">
+                        <p>GL - 380GL001</p>
+                        <div class="full-color">
+                            <div class="hinh-tron color1"></div>
+                            <div class="hinh-tron color2 change-color"></div>
+                            <div class="hinh-tron color3"></div>
+                            <div class="hinh-tron color4"></div>
+                            <div class="hinh-tron color5"></div>
+                        </div>
+                        </div>
+                        <div class="price-glass change-price">
+                           <p>170.000 đ</p>
+                           <p>250.000 đ</p>
+                        </div>
+            </div>
+            </div>
+
+            <div class="swiper-slide">
+            <div class="content-item">
+                        <div class="item-img item3-tablet">
+                           <img src="./styles/img/Rectangle 125.png" alt="">
+                           <div class="discount-item">50%</div>
+                           <div class="img-icon1">
+                               <i class="fa-solid fa-chevron-left"></i>
+                           </div>
+                        </div>
+                        <div class="name-glass name-change">
+                        <p>GL - 380GL001</p>
+                        <div class="full-color">
+                            <div class="hinh-tron color1"></div>
+                            <div class="hinh-tron color2 change-color"></div>
+                            <div class="hinh-tron color3"></div>
+                            <div class="hinh-tron color4"></div>
+                            <div class="hinh-tron color5"></div>
+                        </div>
+                        </div>
+                        <div class="price-glass change-price">
+                           <p>170.000 đ</p>
+                           <p>250.000 đ</p>
+                        </div>
+            </div>
+            </div>
+            <div class="swiper-slide">
+            <div class="content-item">
+                        <div class="item-img item3-tablet">
+                           <img src="./styles/img/Rectangle 125.png" alt="">
+                           <div class="discount-item">50%</div>
+                           <div class="img-icon1">
+                               <i class="fa-solid fa-chevron-left"></i>
+                           </div>
+                        </div>
+                        <div class="name-glass name-change">
+                        <p>GL - 380GL001</p>
+                        <div class="full-color">
+                            <div class="hinh-tron color1"></div>
+                            <div class="hinh-tron color2 change-color"></div>
+                            <div class="hinh-tron color3"></div>
+                            <div class="hinh-tron color4"></div>
+                            <div class="hinh-tron color5"></div>
+                        </div>
+                        </div>
+                        <div class="price-glass change-price">
+                           <p>170.000 đ</p>
+                           <p>250.000 đ</p>
+                        </div>
+            </div>
+            </div>
+            <div class="swiper-slide">
+            <div class="content-item">
+                        <div class="item-img item3-tablet">
+                           <img src="./styles/img/Rectangle 125.png" alt="">
+                           <div class="discount-item">50%</div>
+                           <div class="img-icon1">
+                               <i class="fa-solid fa-chevron-left"></i>
+                           </div>
+                        </div>
+                        <div class="name-glass name-change">
+                        <p>GL - 380GL001</p>
+                        <div class="full-color">
+                            <div class="hinh-tron color1"></div>
+                            <div class="hinh-tron color2 change-color"></div>
+                            <div class="hinh-tron color3"></div>
+                            <div class="hinh-tron color4"></div>
+                            <div class="hinh-tron color5"></div>
+                        </div>
+                        </div>
+                        <div class="price-glass change-price">
+                           <p>170.000 đ</p>
+                           <p>250.000 đ</p>
+                        </div>
+            </div>
+            </div>
+            <div class="swiper-slide">
+            <div class="content-item">
+                        <div class="item-img item3-tablet">
+                           <img src="./styles/img/Rectangle 125.png" alt="">
+                           <div class="discount-item">50%</div>
+                           <div class="img-icon1">
+                               <i class="fa-solid fa-chevron-left"></i>
+                           </div>
+                        </div>
+                        <div class="name-glass name-change">
+                        <p>GL - 380GL001</p>
+                        <div class="full-color">
+                            <div class="hinh-tron color1"></div>
+                            <div class="hinh-tron color2 change-color"></div>
+                            <div class="hinh-tron color3"></div>
+                            <div class="hinh-tron color4"></div>
+                            <div class="hinh-tron color5"></div>
+                        </div>
+                        </div>
+                        <div class="price-glass change-price">
+                           <p>170.000 đ</p>
+                           <p>250.000 đ</p>
+                        </div>
+            </div>
+            </div>
+            <div class="swiper-slide">
+            <div class="content-item">
+                        <div class="item-img item3-tablet">
+                           <img src="./styles/img/Rectangle 125.png" alt="">
+                           <div class="discount-item">50%</div>
+                           <div class="img-icon1">
+                               <i class="fa-solid fa-chevron-left"></i>
+                           </div>
+                        </div>
+                        <div class="name-glass name-change">
+                        <p>GL - 380GL001</p>
+                        <div class="full-color">
+                            <div class="hinh-tron color1"></div>
+                            <div class="hinh-tron color2 change-color"></div>
+                            <div class="hinh-tron color3"></div>
+                            <div class="hinh-tron color4"></div>
+                            <div class="hinh-tron color5"></div>
+                        </div>
+                        </div>
+                        <div class="price-glass change-price">
+                           <p>170.000 đ</p>
+                           <p>250.000 đ</p>
+                        </div>
+            </div>
+            </div>
+            
+            
+
+        </div>
+                <div class="swiper-button-next change-next"></div>
+                <div class="swiper-button-prev change-next"></div>
+                <div class="swiper-pagination"></div>
+                </div>
